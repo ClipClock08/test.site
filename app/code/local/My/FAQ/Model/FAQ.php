dcdc1e5a7069df24217ea2b32e5da7c252871f34
Alexander Lopatin <alexander.lopatin@brander.ua>
2020-06-24 16:54:52 +0300

    change sql script for orm

--- app/code/local/My/FAQ/Model/FAQ.php
+++
@@ -1,8 +0,0 @@
-<?php
-class My_FAQ_Model_FAQ extends Mage_Core_Model_Abstract{
-    public function _construct()
-    {
-        parent::_construct();
-        $this->_init('faq/faq');
-    }
-}
--- app/code/local/My/FAQ/Model/Resource/FAQ.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class My_FAQ_Model_Resource_FAQ extends Mage_Core_Model_Mysql4_Abstract
-{
-    public function _construct()
-    {
-        $this->_init('faq/table_faq', 'id');
-    }
-}
