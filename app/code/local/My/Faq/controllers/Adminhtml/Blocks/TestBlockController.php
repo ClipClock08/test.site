af7108f6fa54add32e9ff316f42c2243129b7f92
Alexander Lopatin <alexander.lopatin@brander.ua>
2020-06-24 16:55:33 +0300

    change sql script for orm

---
+++ app/code/local/My/Faq/controllers/Adminhtml/Blocks/TestBlockController.php
@@ -0,0 +1,9 @@
+<?php
+
+class My_FAQ_Adminhtml_Blocks_TestBlockController extends Mage_Adminhtml_Controller_Action
+{
+    public function mytestAction()
+    {
+        die('here');
+    }
+}
---
+++ app/code/local/My/Faq/controllers/OtherController.php
@@ -0,0 +1,19 @@
+<?php
+
+class My_FAQ_OtherController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo 'Other action';
+    }
+
+    public function addAction()
+    {
+        echo 'add Action';
+    }
+
+    public function deleteAction()
+    {
+        echo 'delete Action';
+    }
+}
